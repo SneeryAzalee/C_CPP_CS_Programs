b1354ca1605ad905b0d1681986bb70965290005f
Sneery <93573665+SneeryAzalee@users.noreply.github.com>
2024-01-08 03:10:29 +0800

    Add files via upload

--- cpe112_1sem_final_written_exam.c
+++ cpe112_1sem_final_written_exam.c
@@ -38,8 +38,7 @@ void swap()
     {
         // Initialize
         char s_input[100];
-        int whole_digit_count = 0, decimal_digit_count = 0, decimal_mode = 0, null_count = 0;
-        double input, whole, decimal;
+        int whole_digit_count = 0, decimal_digit_count = 0, decimal_mode = 0, null_count = 0, whole_start, decimal_start;
         
         // Get input string
         scanf("%s", &s_input);
@@ -63,57 +62,74 @@ void swap()
             // Count whole number digits
             else if(decimal_mode == 0)
             {
-                // If it detects period(.), switch to decimal mode
+                // If it detects period(.), get the point location and switch to decimal mode
                 if(s_input[j] == '.')
                 {
+                    decimal_start = j + 1;
                     decimal_mode = 1;
                 }
                 else if(s_input[j] != '0' || whole_digit_count > 0)
                 {
+                    // Get string starting position
+                    if(whole_digit_count == 0)
+                    {
+                        whole_start = j;
+                    }
+
                     whole_digit_count++;
                 }
             }
         }
-        
-        // Convert input string into double data type
-        sscanf(s_input, "%lf", &input);
-        
-        // Extract whole and decimal values
-        whole = (int)input;
-        decimal = input - (int)input;
         
         // Print output template once
         if(i == 1)
         {
             printf("\nOutput/s  :  ");
         }
-        
+
         // Execute if input has a whole number only
         if(decimal_digit_count == 0)
         {
-            printf("%.0lf ", whole);
+            // Print whole number
+            for(int j = 1; j <= whole_digit_count; j++)
+            {
+                printf("%c", s_input[whole_start++]);
+            }
+
+            // Print separator
+            printf(" ");
         }
         // Execute if input has a decimal number
         else if(decimal_digit_count > 0)
         {
-            // Initialize
-            int digit_limiter = whole_digit_count;
+            // Swap decimal number to whole
+            int allow_print = 0;
+            for(int j = 1; j <= decimal_digit_count; j++)
+            {
+                // Only start printing if it detects a number greater than 0
+                if(s_input[decimal_start] != '0' && allow_print == 0)
+                {
+                    allow_print = 1;
+                }
+                if(allow_print == 1)
+                {
+                    printf("%c", s_input[decimal_start]);
+                }
+                decimal_start++;
+            }
             
             // Swap whole number to decimal
-            while(whole_digit_count > 0)
+            for(int j = 1; j <= whole_digit_count; j++)
             {
-                whole /= 10;
-                whole_digit_count--;
+                if(j == 1)
+                {
+                    printf(".");
+                }
+                printf("%c", s_input[whole_start++]);
             }
-            // Swap decimal number to whole
-            while(decimal_digit_count > 0)
-            {
-                decimal *= 10;
-                decimal_digit_count--;
-            }
-            
-            // Print output
-            printf("%.*lf ", digit_limiter, whole + decimal);
+
+            // Print separator
+            printf(" ");
         }
     }
 }
@@ -152,12 +168,12 @@ void sort()
     {
         // Initialize
         char s_input[100];
-        int decimal_digit_count = 0, decimal_mode = 0, null_count = 0, point_locator;
+        int whole_digit_count = 0, decimal_digit_count = 0, decimal_mode = 0, null_count = 0, whole_start, decimal_start;
         
         // Get input string
         scanf("%s", &s_input);
         
-        // Get digit count of decimal number
+        // Get digit count of whole and decimal numbers
         for(int j = 0; j < strlen(s_input); j++)
         {
             // Count decimal number digits
@@ -173,14 +189,24 @@ void sort()
                     null_count = 0;
                 }
             }
-            // Get decimal point location
+            // Count whole number digits
             else if(decimal_mode == 0)
             {
-                // If it detects period(.), switch to decimal mode
+                // If it detects period(.), get the point location and switch to decimal mode
                 if(s_input[j] == '.')
                 {
-                    point_locator = j;
+                    decimal_start = j + 1;
                     decimal_mode = 1;
+                }
+                else if(s_input[j] != '0' || whole_digit_count > 0)
+                {
+                    // Get string starting position
+                    if(whole_digit_count == 0)
+                    {
+                        whole_start = j;
+                    }
+
+                    whole_digit_count++;
                 }
             }
         }
@@ -194,19 +220,30 @@ void sort()
         // Execute if input has a whole number only
         if(decimal_digit_count == 0)
         {
-            printf("%d ", atoi(s_input));
+            // Print whole number
+            for(int j = 1; j <= whole_digit_count; j++)
+            {
+                printf("%c", s_input[whole_start++]);
+            }
+
+            // Print separator
+            printf(" ");
         }
         // Execute if input has a decimal number
         else if(decimal_digit_count > 0)
         {
             // Print whole number
-            printf("%d.", atoi(s_input));
+            for(int j = 1; j <= whole_digit_count; j++)
+            {
+                printf("%c", s_input[whole_start++]);
+            }
+            printf(".");
 
             // Arrange decimal digits in ascending order
             for(int j = 0; j <= 9; j++)
             {
                 // Initialize
-                int index = point_locator + 1;
+                int index = decimal_start;
                 
                 // Go through each digit
                 for(int k = 1; k <= decimal_digit_count; k++)
@@ -232,7 +269,173 @@ void sort()
 // Problem 3
 void delete()
 {
-    printf("3");
+    /*
+        Write a program that will ask for n numbers. Your program
+        will then delete the duplicate digit(s) found in the
+        integer and fractional of your input number.
+
+        Example:
+
+            Enter number of inputs  :  4
+            Enter inputs            :  191.976  191  123.434  0.123
+            Visualization           :  1 1. 76  191  12 .4 4  0.123
+            Output                  :  11.76    191  12.44    0.123
+    */
+
+    // Print information
+    printf("\n\nYou have selected Problem 3:");
+    printf("\nThis program will remove similar digits found in both whole and decimal numbers.");
+    printf("\n\nEx.  123.424  >>  13.44\n\n");
+    
+    // Initialize
+    int input_count;
+
+    // Ask how many inputs
+    printf("\nEnter number of inputs  :  ");
+    scanf("%d", &input_count);
+    printf("\nInput/s   :  ");
+
+    // Loop through all the inputs
+    for(int i = 1; i <= input_count; i++)
+    {
+        // Initialize
+        char s_input[100];
+        int whole_digit_count = 0, decimal_digit_count = 0, decimal_mode = 0, null_count = 0, whole_start, decimal_start;
+        
+        // Get input string
+        scanf("%s", &s_input);
+        
+        // Get digit count of whole and decimal numbers
+        for(int j = 0; j < strlen(s_input); j++)
+        {
+            // Count decimal number digits
+            if(decimal_mode == 1)
+            {
+                if(s_input[j] == '0')
+                {
+                    null_count++;
+                }
+                else
+                {
+                    decimal_digit_count += null_count + 1;
+                    null_count = 0;
+                }
+            }
+            // Count whole number digits
+            else if(decimal_mode == 0)
+            {
+                // If it detects period(.), get the point location and switch to decimal mode
+                if(s_input[j] == '.')
+                {
+                    decimal_start = j + 1;
+                    decimal_mode = 1;
+                }
+                else if(s_input[j] != '0' || whole_digit_count > 0)
+                {
+                    // Get string starting position
+                    if(whole_digit_count == 0)
+                    {
+                        whole_start = j;
+                    }
+                    whole_digit_count++;
+                }
+            }
+        }
+        
+        // Print output template once
+        if(i == 1)
+        {
+            printf("\nOutput/s  :  ");
+        }
+
+        // Initialize dynamic array for whole and decimal digits
+        char whole_digits[whole_digit_count + 1];
+        char decimal_digits[decimal_digit_count + 1];
+        int index;
+
+        // Store whole number digits
+        index = whole_start;
+        for(int j = 1; j <= whole_digit_count; j++)
+        {
+            whole_digits[j] = s_input[index++];
+        }
+
+        // Store decimal number digits
+        index = decimal_start;
+        for(int j = 1; j <= decimal_digit_count; j++)
+        {
+            decimal_digits[j] = s_input[index++];
+        }
+
+        // Mark similar digits found in both whole and decimal digits
+        for(int j = 1; j <= whole_digit_count; j++)
+        {
+            for(int k = 1; k <= decimal_digit_count; k++)
+            {
+                if(whole_digits[j] == decimal_digits[k])
+                {
+                    whole_digits[j] = '-';
+                    decimal_digits[k] = '-';
+                    break;
+                }
+            }
+        }
+
+        // Execute if whole number has no value
+        if(whole_digit_count == 0)
+        {
+            printf("0");
+            // Print decimal digits (if any)
+            for(int j = 1; j <= decimal_digit_count; j++)
+            {
+                if(j == 1)
+                {
+                    printf(".");
+                }
+                printf("%c", decimal_digits[j]);
+            }
+
+            // Print separator
+            printf(" ");
+        }
+        // Execute if whole number has a value
+        else if(whole_digit_count > 0)
+        {
+            // Print whole digits
+            decimal_mode = 1;
+            for(int j = 1; j <= whole_digit_count; j++)
+            {
+                // If all digits in whole number are marked, print 0
+                if(whole_digits[j] == '-' && j == whole_digit_count && decimal_mode == 1)
+                {
+                    printf("0");
+                }
+                else if(whole_digits[j] != '-')
+                {
+                    printf("%c", whole_digits[j]);
+                    decimal_mode = 0;
+                }
+            }
+
+            // Print decimal digits (if any)
+            decimal_mode = 0;
+            for(int j = 1; j <= decimal_digit_count; j++)
+            {
+                if(decimal_digits[j] != '-')
+                {
+                    if(decimal_mode == 0)
+                    {
+                        printf(".");
+                        decimal_mode = 1;
+                    }
+                    printf("%c", decimal_digits[j]);
+                }
+            }
+
+            // Print separator
+            printf(" ");
+        }
+    }
 }
 
 
@@ -247,7 +450,7 @@ int main()
     printf("\nPlease select a problem:\n");
     printf("\n1) Integer-Decimal number swapper");
     printf("\n2) Decimal number ascending arrangement");
-    printf("\n3) Delete duplicate digits in whole and decimal numbers");
+    printf("\n3) Delete similar digits in whole and decimal numbers");
     printf("\n\n>>  ");
     scanf("%d", &selector);
 
