e8117c4fd6ae87eb79b40be333b1d67dc5f971d7
Sneery <93573665+SneeryAzalee@users.noreply.github.com>
2024-09-06 06:59:09 +0800

    Add files via upload

--- prime_list.c
+++ prime_list.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Make a program that will list the prime numbers.
+
+void is_Prime(int limit)
+{
+    int count = 1, num = 2;
+    while(count <= limit)
+    {
+        int prime = 1, output;
+        for(int i = 2; i < num; i++)
+        {
+            output = num % i;
+            if(output == 0)
+            {
+                prime = 0;
+                break;
+            }
+        }
+        if(prime == 1)
+        {
+            printf("\n%d. %d", count, num);
+            count++;
+        }
+        num++;
+    }
+}
 
 int main()
 {
-    int limit, iteration = 1;
-    printf("\n\nEnter limit: ");
-    scanf("%d", &limit);
+    printf("\nEnter limit: ");
+    int input;
+    scanf("%d", &input);
     printf("\n");
-    
-    for(long long i = 2L; iteration <= limit; i++)
-    {
-        int count = 0;
-        for(long long j = 2L; j < i; j++)
-        {
-            if(i % j == 0L)
-            {
-                count++;
-            }
-        }
-        if(count == 0)
-        {
-            printf("%d. %lld\n", iteration, i);
-            iteration++;
-        }
-    }
+
+    is_Prime(input);
+    printf("\n");
+
     return 0;
 }
--- test.c
+++ test.c
@@ -1,54 +1,142 @@
 #include <stdio.h>
-#include <string.h>
 
-int main()
+void prob1()
 {
-    char str[100];
-    int x, w_count = 0, d_count = 0, deci_mode = 0;
-    double y, whole, decimal;
-    
-    scanf("%s", str);
-    
-    for(int i = 0; i < strlen(str); i++)
+    for(int i = 0; i < 5; i++)
     {
-        if(deci_mode == 0)
+        for(int j = 5; j > i; j--)
         {
-            if(str[i] == '.')
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+void prob2()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if(j < i)
             {
-                deci_mode = 1;
+                printf("  ");
             }
             else
             {
-                w_count++;
+                printf("* ");
             }
         }
-        else if(deci_mode == 1)
+        printf("\n");
+    }
+}
+
+void prob3()
+{
+    for(int i = 5; i > 0; i--)
+    {
+        for(int j = 0; j < 5; j++)
         {
-            d_count++;
+            if(j > (i - 2))
+            {
+                printf("* ");
+            }
+            else
+            {
+                
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void prob4()
+{
+    int input, output = 0, storage = 2;
+    
+    printf("Input: ");
+    scanf("%d", &input);
+    printf("\n1 • 1 ");
+    
+    for(int i = 3; i <= input; i++)
+    {
+        if(i % 2 == 0)
+        {
+            output *= 2;
+            printf("• %d ", output);
+            storage++;
+        }
+        else
+        {
+            output += storage;
+            printf("• %d ", output);
         }
     }
+    printf("\n");
+}
+
+void prob5()
+{
+    int input, previous_odd = 1, previous_even = 1, delay = 2;
     
-    sscanf(str, "%lf", &y);
+    printf("Input: ");
+    scanf("%d", &input);
+    printf("\n1 • 1 ");
     
-    whole = (int)y;
-    decimal = y - whole;
-    
-    int temp = w_count;
-    
-    while(w_count > 0)
+    for(int i = 3; i <= input; i++)
     {
-        whole /= 10;
-        w_count--;
+        if(i % 2 == 0)
+        {
+            previous_even += 4;
+            printf("• %d ", previous_even);
+        }
+        else
+        {
+            if(delay == 2)
+            {
+                delay = 0;
+                previous_odd += 1;
+            }
+            printf("• %d ", previous_odd);
+            delay++;
+        }
     }
-    
-    while(d_count > 0)
+    printf("\n");
+}
+
+int main()
+{
+    int selector;
+
+    printf("\nSelect Problem (1, 2, 3, 4, 5):\n\n>> ");
+    scanf("%d", &selector);
+    printf("\nOutput:\n\n");
+
+    if(selector == 1)
     {
-        decimal *= 10;
-        d_count--;
+        prob1();
     }
-    
-    int final = whole + decimal;
-    
-    
-    printf("\n%.*lf", temp, final);
+    else if(selector == 2)
+    {
+        prob2();
+    }
+    else if(selector == 3)
+    {
+        prob3();
+    }
+    else if(selector == 4)
+    {
+        prob4();
+    }
+    else if(selector == 5)
+    {
+        prob5();
+    }
+    else
+    {
+        printf("\nInvalid input!\n\n");
+    }
+
+    return 0;
 }
